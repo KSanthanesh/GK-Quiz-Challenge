aaf53e0a4cf3fc468488ec1bacc3a776d8432413
KSanthanesh <santhanesh_2004@yahoo.com>
2021-07-13 21:46:32 +0000

    add valid option to enter using if condition

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ while True:
     if not name:
         print("Please Enter your Name before start: ")
     else:
-        print("Hi", name, "Let's start the Game")
+        print("Hi", name, "Let's start the Game\n")
         break
 
 
@@ -41,15 +41,19 @@ def start():
     question_num = 1
 
     for key in questions:
-        print("****************************************************")
+        print("**********************************************************")
         print(key)
         for i in options[question_num - 1]:
             print(i)
-        choice = input("Enter Your Answer (A,B,C or D): ")
-        choice = choice.upper
+        while True:
+            choice = input("Enter Your Answer (A,B,C or D): ")
+            if choice not in ("A", "B", "C", "D", "a", "b", "c", "d"):
+                print("Please Enter a Valid options")
+            else:
+                break
+        choice = choice.upper()
         choices.append(choice)
         correct_choices += check_answer(questions.get(key), choice)
-
         question_num += 1
     display_score(correct_choices, choices)
 
@@ -70,13 +74,13 @@ def display_score(correct_choices, choices):
     """
     Score display
     """
-    print("*****------------------------------------*****")
+    print("-----------------------------------------------")
     print("Quiz Results")
-    print("*****-------------------------------------*****")
+    print("-----------------------------------------------")
 
 
 questions = {
-    "1. which Country is known as the 'land of Raising Sun'?\n ": "A",
+    "1. What is the square root of 144?\n ": "A",
     "2. Which Country is known as the 'Playground of Europe'?\n": "C",
     "3. What percentage of the human body is water?\n": "B",
     "4. What is the hottest Chilli Pepper in the World?\n": "A",
@@ -85,7 +89,7 @@ questions = {
     "7. What is the popular spice in the world?\n": "A",
     "8. What kind of tree do acorns grow on?\n": "C",
     "9. Which Country has the most Volcanoes?\n": "D",
-    "10. What is the most spoken language in the world?\n": "B",
+    "10. Which country is the largest producer of Coffee??\n": "B",
     "11. Which is the hottest planet is the Solar system?\n": "D",
     "12. Which European Country was the first to allow women to vote?\n": "A",
     "13. What is the degree of triangle?\n": "C",
@@ -94,18 +98,19 @@ questions = {
 
 }
 
+
 options = [
-    ["A. Japan", "B. New Zealand", "C. Fiji", "D. China\n"],
-    ["A. Austria", "B. France", "C. Switzerland", "D. Italy"],
+    ["A. 12", "B. 22", "C. 14", "D. 11\n"],
+    ["A. Austria", "B. France", "C. Switzerland", "D. Italy\na"],
     ["A. 75%", "B. 60%", "C. 69%", "D. 65%\n"],
-    ["A. The Carolina Reaper", "B. Ghost Pepper", "C. Pot Barrackpore"
+    ["A. The Carolina Reaper", "B. Ghost Pepper", "C. Pot Barrackpore",
      "D. Pot Red\n"],
     ["A. China", "B. India", "C. Africa", "D. Russia\n"],
     ["A. Four", "B. Six", "C. Eight", "D. Two\n"],
     ["A. Pepper", "B. Paprika", "C. Thyme", "D. Chilli\n"],
     ["A. Ash", "B. Birch", "C. Oak", "D. Rowan\n"],
     ["A. Japan", "B. Russia", "C. Chile", "D. Indonesia\n"],
-    ["A. English", "B. Chinese", "C. German", "D. French\n"],
+    ["A. Vietnam", "B. Brazil", "C. Colombia", "D. Ethiopia\n"],
     ["A. Jupitar", "B. Mars", "C. Mercury", "D. Venus\n"],
     ["A. Finland", "B. Germany", "C. France", "D. Ireland\n"],
     ["A. 240 degree", "B. 360 degree", "C. 180 degree", "D. 90 degree\n"],
