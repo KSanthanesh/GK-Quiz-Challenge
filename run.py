abc4b0ad1e087fb668e16ec8bbbb26d86489c62d
KSanthanesh <santhanesh_2004@yahoo.com>
2021-07-13 16:46:47 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
-# python code goes here
+# Heading
+print("\n")
+print("               *************************")
+print("               ****                 ****")
+print("               **  GK Quiz Challenge  **")
+print("               ****                 ****")
+print("               *************************")
+
+# Welcome message
+print("\nWelcome to my Computer Quiz\n")
+
+confirm = input("Do you want to play a Game?(yes/no): ")
+confirm = confirm.upper()
+
+if confirm in ("Y", "YES"):
+    pass
+else:
+    print("Thank You! Come back later")
+    quit()
