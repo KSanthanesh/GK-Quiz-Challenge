e9720e6731f6f4268bc1ac113b51961a2c7b062a
KSanthanesh <santhanesh_2004@yahoo.com>
2021-07-13 17:20:41 +0000

    Add name details and the game doesn't start without name details

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ print("               *************************")
 # Welcome message
 print("\nWelcome to my Computer Quiz\n")
 
+# confirmation whether the user want to play the game
 confirm = input("Do you want to play a Game?(yes/no): ")
 confirm = confirm.upper()
 
@@ -17,3 +18,15 @@ if confirm in ("Y", "YES"):
 else:
     print("Thank You! Come back later")
     quit()
+
+"""
+Enter Name of the user, Game doesnot start without
+Name details.
+"""
+while True:
+    name = input("Please Enter Your Name: ")
+    if not name:
+        print("Please Enter your Name before start: ")
+    else:
+        print("Hi", name, "Let's start the Game")
+        break
