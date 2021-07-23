8cddcc9d03224706fa230e910abead0b3a8fa011
KSanthanesh <santhanesh_2004@yahoo.com>
2021-07-23 21:59:47 +0000

    Delete score.py file and use capital letter for global varaible

--- run.py
+++ run.py
@@ -2,12 +2,10 @@
 Multiple choice question and answers game
 """
 import sys
-from termcolor import colored
 import time
+from termcolor import colored
 import gspread
 from google.oauth2.service_account import Credentials
-# from pprint import pprint
-
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,7 +18,51 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Gk_Quiz_Challenge')
 
-score_details = SHEET.worksheet('score_details')
+SCORE_DETAILS = SHEET.worksheet('score_details')
+
+
+# questions and correct answer in dictionaries method
+QUESTIONS = {
+    '1. What is the square root of 144?\n ': 'A',
+    "2. Which Country is known as the 'Playground of Europe'?\n": 'C',
+    '3. What percentage of the human body is water?\n': 'B',
+    '4. What is the hottest Chilli Pepper in the World?\n': 'A',
+    '5. Which Country has the biggest Land Area?\n': 'D',
+    '6. How many legs does a butterfly have?\n': 'B',
+    '7. What is the popular spice in the world?\n': 'A',
+    '8. What kind of tree do acorns grow on?\n': 'C',
+    '9. Which Country has the most Volcanoes?\n': 'D',
+    '10. Which country is the largest producer of Coffee??\n': 'B',
+    '11. Which is the hottest planet is the Solar system?\n': 'D',
+    '12. Which European Country was the first to allow women to vote?\n':
+    'A',
+    '13. What is the degree of triangle?\n': 'C',
+    '14. What is the Chemical symbol for table salt?\n': 'D',
+    '15. What is the largest three digit prime Number?\n': 'B'
+
+}
+
+
+# multiple choice answers in list methods
+OPTIONS = [
+    ['A. 12', 'B. 22', 'C. 14', 'D. 11\n'],
+    ['A. Austria', 'B. France', 'C. Switzerland', 'D. Italy\n'],
+    ['A. 75%', 'B. 60%', 'C. 69%', 'D. 65%\n'],
+    ['A. The Carolina Reaper', 'B. Ghost Pepper', 'C. Pot Barrackpore',
+     'D. Pot Red\n'],
+    ['A. China', 'B. India', 'C. Africa', 'D. Russia\n'],
+    ['A. Four', 'B. Six', 'C. Eight', 'D. Two\n'],
+    ['A. Pepper', 'B. Paprika', 'C. Thyme', 'D. Chilli\n'],
+    ['A. Ash', 'B. Birch', 'C. Oak', 'D. Rowan\n'],
+    ['A. Japan', 'B. Russia', 'C. Chile', 'D. Indonesia\n'],
+    ['A. Vietnam', 'B. Brazil', 'C. Colombia', 'D. Ethiopia\n'],
+    ['A. Jupitar', 'B. Mars', 'C. Mercury', 'D. Venus\n'],
+    ['A. Finland', 'B. Germany', 'C. France', 'D. Ireland\n'],
+    ['A. 240 degree', 'B. 360 degree', 'C. 180 degree', 'D. 90 degree\n'],
+    ['A. Sodium hydroxide', 'B. Sodium bicarbonate', 'C. Sodium Carbonate',
+     'D. Sodium Chloride\n'],
+    ['A. 995', 'B. 997', 'C. 999', 'D. 993\n'],
+]
 
 
 # Heading
@@ -49,7 +91,8 @@ time.sleep(0.5)
 while True:
     username = input('Please Enter Your Name: \n').capitalize()
     if not username:
-        print('Please Enter your Name before start: \n')
+        print(colored("Please Enter your Name before start: \n", 'red',
+                      attrs=['bold']))
     else:
         print("Hi", username, "Let's start the Game")
         print('There are 15 questions to answer. Best of Luck!!!\n')
@@ -65,22 +108,23 @@ def start():
     correct_choices = 0
     question_num = 1
 
-    for key in questions:
+    for key in QUESTIONS:
         print(colored("******************************************************",
                       'cyan', attrs=['bold']))
         print(key)
-        for i in options[question_num - 1]:
+        for i in OPTIONS[question_num - 1]:
             print(i)
 
         while True:
             choice = input('Enter Your Answer (A,B,C or D): \n')
             if choice not in ('A', 'B', 'C', 'D', 'a', 'b', 'c', 'd'):
-                print("Please Enter a Valid options")
+                print(colored("Please Enter a Valid options", 'red',
+                              attrs=['bold']))
             else:
                 break
         choice = choice.upper()
         choices.append(choice)
-        correct_choices += check_answer(questions.get(key), choice)
+        correct_choices += check_answer(QUESTIONS.get(key), choice)
         question_num += 1
     display_score(correct_choices)
 
@@ -93,9 +137,10 @@ def check_answer(answer, choice):
     Feedback given for right and wrong answer
     """
     if answer == choice:
-        print('\n Well done! Correct Answer!\n')
+        print(colored("\n Well done! Correct Answer!\n", 'green',
+                      attrs=['bold']))
         return True
-    print('\n Incorrect Answer\n')
+    print(colored("\n Incorrect Answer\n", 'red', attrs=['bold']))
     return False
 
 
@@ -104,26 +149,28 @@ def display_score(correct_choices):
     Score display in terms of number of questions been answer correctly.
     And Calculate the score in percentage too
     """
-    print(colored("*******************************************************"
+    print(colored("*******************************************************",
                   'cyan', attrs=['bold']))
-    print('             ---------------------------')
-    print('             |       Quiz Results      |')
-    print('             ---------------------------\n')
+    print(colored("    ---------------------------", 'cyan', attrs=['bold']))
+    print(colored("    |       Quiz Results      |", 'white', attrs=['bold']))
+    print(colored("    ---------------------------\n", 'cyan', attrs=['bold']))
+
     score = int(correct_choices)
     print('Thank you for playing! You got', score, '/',
-          len(questions), 'questions correct.')
-    mark = int(score/len(questions) * 100)
+          len(QUESTIONS), 'questions correct.')
+    mark = int(score/len(QUESTIONS) * 100)
     print('Hi', username, ', Your Score is:', str(mark), '%.\n')
     marks = mark / 100
 
     # Update User's name and score in google sheet
     user = [marks, score, username]
-    score_details.append_row(user)
-    results = score_details.get_all_values()
-    print("Top 3 Scores:\n")
+    SCORE_DETAILS.append_row(user)
+    results = SCORE_DETAILS.get_all_values()
+    print(colored("Top 3 Scores:\n", 'yellow', attrs=['bold']))
     result_1 = sorted(results[1:], reverse=True)[:3]
     for result in result_1:
-        print(result[2] + "'s Score is " + result[0])
+        print(colored(result[2] + "'s Score is " + result[0],
+              'yellow', attrs=['bold']))
 
 
 time.sleep(1)
@@ -139,52 +186,9 @@ def play_again():
         return True
     if response in ('N', 'NO'):
         return False
-    print('Please Enter Valid option (yes or no).')
+    print(colored("Please Enter Valid option (yes or no).",
+                  'red', attrs=['bold']))
     return play_again()
-
-
-# questions and correct answer in dictionaries method
-questions = {
-    '1. What is the square root of 144?\n ': 'A',
-    "2. Which Country is known as the 'Playground of Europe'?\n": 'C',
-    '3. What percentage of the human body is water?\n': 'B',
-    '4. What is the hottest Chilli Pepper in the World?\n': 'A',
-    '5. Which Country has the biggest Land Area?\n': 'D',
-    '6. How many legs does a butterfly have?\n': 'B',
-    '7. What is the popular spice in the world?\n': 'A',
-    '8. What kind of tree do acorns grow on?\n': 'C',
-    '9. Which Country has the most Volcanoes?\n': 'D',
-    '10. Which country is the largest producer of Coffee??\n': 'B',
-    '11. Which is the hottest planet is the Solar system?\n': 'D',
-    '12. Which European Country was the first to allow women to vote?\n':
-    'A',
-    '13. What is the degree of triangle?\n': 'C',
-    '14. What is the Chemical symbol for table salt?\n': 'D',
-    '15. What is the largest three digit prime Number?\n': 'B'
-
-}
-
-
-# multiple choice answers in list methods
-options = [
-    ['A. 12', 'B. 22', 'C. 14', 'D. 11\n'],
-    ['A. Austria', 'B. France', 'C. Switzerland', 'D. Italy\n'],
-    ['A. 75%', 'B. 60%', 'C. 69%', 'D. 65%\n'],
-    ['A. The Carolina Reaper', 'B. Ghost Pepper', 'C. Pot Barrackpore',
-     'D. Pot Red\n'],
-    ['A. China', 'B. India', 'C. Africa', 'D. Russia\n'],
-    ['A. Four', 'B. Six', 'C. Eight', 'D. Two\n'],
-    ['A. Pepper', 'B. Paprika', 'C. Thyme', 'D. Chilli\n'],
-    ['A. Ash', 'B. Birch', 'C. Oak', 'D. Rowan\n'],
-    ['A. Japan', 'B. Russia', 'C. Chile', 'D. Indonesia\n'],
-    ['A. Vietnam', 'B. Brazil', 'C. Colombia', 'D. Ethiopia\n'],
-    ['A. Jupitar', 'B. Mars', 'C. Mercury', 'D. Venus\n'],
-    ['A. Finland', 'B. Germany', 'C. France', 'D. Ireland\n'],
-    ['A. 240 degree', 'B. 360 degree', 'C. 180 degree', 'D. 90 degree\n'],
-    ['A. Sodium hydroxide', 'B. Sodium bicarbonate', 'C. Sodium Carbonate',
-     'D. Sodium Chloride\n'],
-    ['A. 995', 'B. 997', 'C. 999', 'D. 993\n'],
-]
 
 
 start()
